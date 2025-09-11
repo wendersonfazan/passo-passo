338469b7146eb07dc4ff1969570b89baaa8bd631
Wenderson Fazan <wenderson.fazan@ctinet.com.br>
2025-09-11 12:43:10 -0300

    ajustes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,9 @@
             <div class="flex">
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800"/>
+                        <a href="/">
+                            <img src="{{asset('assets/logosemfundo.png')}}" alt="logo" class="w-8 h-8 fill-current text-gray-500">
+                        </a>
                     </a>
                 </div>
 
